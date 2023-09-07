8afe67817f0dbb31b3cbd6b932947b4cec0668e2
Himadri Chakraborty <himadric@amdocs.com>
2023-09-07 16:40:47 +0530

    regular

---
+++ src/com/F_Interface.java
@@ -0,0 +1,6 @@
+package com;
+
+@FunctionalInterface
+public interface F_Interface {
+    public int multiply(int a,int b) ;
+}
--- src/com/Main.java
+++ src/com/Main.java
@@ -11,15 +11,22 @@ import com.Resources.ROLE;
 public class Main {
 
     private static User admin = new User(ROLE.ADMIN, "100", "ADMIN", "7005810919", "admin@amdocs.com", new Address(null, null, null, null), new Date()) ;  
+    private static List<User> customers ; 
+
 
     public static void main(String[] args) {
         System.out.println("\n************* Welcome to Electricity Billing System *************\n");
-        
+        customers= new ArrayList<>() ; 
         startSystem();
+ 
 
     }
 
     public static void startSystem() {
-        System.out.println("start");
+        loginUser() ; 
+    }
+
+    private static void loginUser() {
+        
     }
 }
