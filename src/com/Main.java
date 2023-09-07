ed2f8ce98da0726dafb43f0a3e7af5726731284b
Himadri Chakraborty <himadric@amdocs.com>
2023-09-07 15:35:41 +0530

    added readme

--- src/com/Main.java
+++ src/com/Main.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import com.Models.Bill;
+import com.Models.Address;
 import com.Models.User;
 import com.Resources.ROLE;
 
 public class Main {
 
-    private static User admin = new User(ROLE.ADMIN, "100", "ADMIN", "7005810919", "admin@amdocs.com", "address", new Date(), null) ;  
+    private static User admin = new User(ROLE.ADMIN, "100", "ADMIN", "7005810919", "admin@amdocs.com", new Address(null, null, null, null), new Date()) ;  
 
     public static void main(String[] args) {
         System.out.println("\n************* Welcome to Electricity Billing System *************\n");
